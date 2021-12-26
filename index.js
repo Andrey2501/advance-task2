1879e86eb6b1477c91ff97eecb61d0bce23f7f2c
Andrii Hranytsia <andrei.granitca@gmail.com>
2021-12-26 23:01:08 +0200

    add logic

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
 
+function fact(num) {
+   let res = 1n;
+   for (let i = 1n; i <= num; i++) {
+      res *= i;
+   }
+
+   return res;
+}
+
 app.get('/', function (req, res) {
-   res.send('Test docker');
+   let startTime = +new Date();
+   const result = fact(req.query['number']);
+   let endTime = +new Date();
+   res.send(`Result: ${result} <br> Computation time: ${endTime - startTime} ms`);
 });
 
 app.listen(3000)
